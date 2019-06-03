f76c9ac5bd37078b78a5f51189b0f4daf5850982
bud93411 <bud93411@gmail.com>
2019-06-03 10:10:20 +0300

    Formatting indentation issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Header = ({title}) => {
-  return (<h2>
-    {title}
-  </h2>);
+const Header = ({ title }) => {
+	return <h1>{title}</h1>;
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-// IMPORT PACKAGE REFERENCES
-
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-// IMPORT PROJECT REFERENCES
 
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 
-// RENDER ROUTER
 const store = configureStore();
 
 render(
   <Router>
     <Root store={store} />
   </Router>,
-  document.getElementById( 'root' ),
+  document.getElementById('root'),
 );
 
 registerServiceWorker();
